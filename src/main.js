10a2669657b538c8a981e040b00fb36cebcdb483
Jeremie Litzler <jeremie.litzler@conduent.com>
2021-06-18 12:03:17 +0200

    fix css path

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import '../public/css/bootstrap4/css/bootstrap.min.css"
-import '../styles.css' 
+import '../public/css/bootstrap4/css/bootstrap.min.css';
+import '../styles.css';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
