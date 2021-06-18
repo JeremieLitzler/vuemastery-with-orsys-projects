b99953559ae71221f4a57539b62f7b8fe17ab0b3
Jeremie Litzler <jeremie.litzler@conduent.com>
2021-06-18 14:14:38 +0200

    rename import + main.js for routes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './routes/main';
+import router from './routes'; //load by default the routes/index.js
 
 import '../public/css/bootstrap4/css/bootstrap.min.css';
 import '../public/css/styles.css';
